d24b33bb1c3119adfae96aa222322f6d47520435
JustShush <marquesdinis.p@gmail.com>
2023-04-11 23:26:16 +0100

    my solution to the test :)

--- 1_ano/2_semestre_IAED/testes/1_teste/main.c
+++ 1_ano/2_semestre_IAED/testes/1_teste/main.c
@@ -1,117 +1,324 @@
-#include "stdio.h"
-# include <stdlib.h>
-#include <stdint.h> // this is for the uint64_t
+#include <stdio.h>
+#include <stdlib.h>
 
-#define MAX_RECORDS 1000
-#define MAX_LINE_LENGTH 2048
+#define MAX_LINE 2048
 
-typedef struct data_st
+typedef struct covid_date_s
 {
-	int day;
-	int month;
-	int year;
-} data_t;
+	int dia;
+	int mes;
+	int ano;
+} covid_date_t;
 
-typedef struct covid_st
+typedef struct covid_data_t
 {
-	data_t date;
-	int confirmados;			 // (coluna 3)
-	int confirmados_arsnorte;	 // (coluna 4)
-	int confirmados_arscentro;	 // (coluna 5)
-	int confirmados_arslvt;		 // (coluna 6)
-	int confirmados_arsalentejo; // (coluna 7)
-	int confirmados_arsalgarve;	 // (coluna 8)
-	int confirmados_acores;		 // (coluna 9)
-	int confirmados_madeira;	 // (coluna 10)
-} covid_t;
+	covid_date_t data;
+	int confirmados;
+	int confirmados_arsnorte;
+	int confirmados_arscentro;
+	int confirmados_arslvt;
+	int confirmados_arsalentejo;
+	int confirmados_arsalgarve;
+	int confirmados_acores;
+	int confirmados_madeira;
+} covid_data_t;
 
-int parse_file(FILE *csv_file, covid_t *records)
+void red() {
+	printf("\033[1;91m");
+}
+
+void yellow() {
+	printf("\033[1;33m");
+}
+
+void blue() {
+	printf("\033[0;34m");
+}
+
+void white() {
+	printf("\033[0;37m");
+}
+
+void info() {
+	printf("\033[0;36m");
+}
+
+void reset() {
+	printf("\033[0m");
+}
+
+int parse_covid_data(const char *filename, covid_data_t **covid_data_lst[], int *num_cases)
 {
-	// Already opened the file
-	char line[MAX_LINE_LENGTH] = {0};
-	fgets(line, MAX_LINE_LENGTH, csv_file); // Skip first line
-	int num_records = 0;
-	while (fgets(line, MAX_LINE_LENGTH, csv_file) != NULL)
+	FILE *fp = fopen(filename, "r");
+	if (fp == NULL)
 	{
-		if (sscanf(line, "%d-%d-%d,%*[^,],%d,%d,%d,%d,%d,%d,%d,%d,%*[^\n]\n",
-				   &(records[num_records].date.day),
-				   &(records[num_records].date.month),
-				   &(records[num_records].date.year),
-				   &(records[num_records].confirmados),
-				   &(records[num_records].confirmados_arsnorte),
-				   &(records[num_records].confirmados_arscentro),
-				   &(records[num_records].confirmados_arslvt),
-				   &(records[num_records].confirmados_arsalentejo),
-				   &(records[num_records].confirmados_arsalgarve),
-				   &(records[num_records].confirmados_acores),
-				   &(records[num_records].confirmados_madeira)) == 11)
+		red();
+		printf("Erro na abertura do arquivo.\nPor favor verificar se o nome do ficheiro esta correto.\n");
+		reset();
+		exit(0);
+	}
+
+	char linha[MAX_LINE] = {0};
+	covid_data_t *new_list;
+	int days_w_cases = 0;
+
+	// skip first line
+	fgets(linha, MAX_LINE, fp);
+
+	while (fgets(linha, MAX_LINE, fp) != NULL)
+	{
+		new_list = (covid_data_t *)malloc(sizeof(covid_data_t));
+		sscanf(linha, "%d-%d-%d,%*[^,],%d,%d,%d,%d,%d,%d,%d,%d,%*[^\n]\n",
+			   &(new_list->data.dia),
+			   &(new_list->data.mes),
+			   &(new_list->data.ano),
+			   &(new_list->confirmados),
+			   &(new_list->confirmados_arsnorte),
+			   &(new_list->confirmados_arscentro),
+			   &(new_list->confirmados_arslvt),
+			   &(new_list->confirmados_arsalentejo),
+			   &(new_list->confirmados_arsalgarve),
+			   &(new_list->confirmados_acores),
+			   &(new_list->confirmados_madeira));
+		*covid_data_lst = realloc(*covid_data_lst, sizeof(covid_data_t *) * (days_w_cases + 1));
+		(*covid_data_lst)[days_w_cases] = new_list;
+		new_list = NULL;
+		days_w_cases++;
+	}
+	*covid_data_lst = realloc(*covid_data_lst, sizeof(covid_data_t *) * (days_w_cases + 1));
+	(*covid_data_lst)[days_w_cases] = NULL;
+	*num_cases = days_w_cases;
+	return 0;
+}
+
+int peak_day(covid_data_t *covid_data_lst[], int cases, int *max_dia, int *max_mes, int *max_ano, int *max_cases_covid, int idx)
+{
+	int ant_peak_day = 0;
+
+	switch (idx)
+	{
+	case 1:
+	{
+		for (int i = 1; i < cases; ++i)
 		{
-			num_records++;
-			if (num_records > MAX_RECORDS)
+			int peak_day = covid_data_lst[i]->confirmados - covid_data_lst[i - 1]->confirmados;
+			if (peak_day > ant_peak_day && i > 0)
 			{
-				printf("Too many records, not storing the remaining part.\n");
-				return MAX_RECORDS;
+				ant_peak_day = peak_day;
+				*max_dia = covid_data_lst[i]->data.dia;
+				*max_mes = covid_data_lst[i]->data.mes;
+				*max_ano = covid_data_lst[i]->data.ano;
+				*max_cases_covid = covid_data_lst[i]->confirmados - covid_data_lst[i - 1]->confirmados;
 			}
 		}
+		break;
 	}
-	return num_records;
-}
-void display(covid_t *records, int num_records)
-{
-	for (int i = 0; i < num_records; i++)
+	case 2:
 	{
-		printf("[%02d-%02d-%04d] Confirmados: %d\n",
-			   records[i].date.day,
-			   records[i].date.month,
-			   records[i].date.year,
-			   records[i].confirmados);
-	}
-}
-void print_peak_day(covid_t *records, int num_records)
-{
-	int peak_idx = 0;
-	int lowest = 0;
-	int real = 0;
-	for (int i = 0; i < num_records; i++)
-	{
-		if (records[peak_idx].confirmados < records[i].confirmados)
+		for (int i = 1; i < cases; ++i)
 		{
-			lowest = records[i].confirmados - records[peak_idx].confirmados;
-			if(lowest > real) {
-				real = lowest;
-				//printf("lowest: %d\n", real);
+			int peak_day = covid_data_lst[i]->confirmados_arsnorte - covid_data_lst[i - 1]->confirmados_arsnorte;
+			if (peak_day > ant_peak_day && i > 0)
+			{
+				ant_peak_day = peak_day;
+				*max_dia = covid_data_lst[i]->data.dia;
+				*max_mes = covid_data_lst[i]->data.mes;
+				*max_ano = covid_data_lst[i]->data.ano;
+				*max_cases_covid = covid_data_lst[i]->confirmados_arsnorte - covid_data_lst[i - 1]->confirmados_arsnorte;
 			}
-			peak_idx = i;
 		}
+		break;
 	}
+	case 3:
+	{
+		for (int i = 1; i < cases; ++i)
+		{
+			int peak_day = covid_data_lst[i]->confirmados_arscentro - covid_data_lst[i - 1]->confirmados_arscentro;
+			if (peak_day > ant_peak_day && i > 0)
+			{
+				ant_peak_day = peak_day;
+				*max_dia = covid_data_lst[i]->data.dia;
+				*max_mes = covid_data_lst[i]->data.mes;
+				*max_ano = covid_data_lst[i]->data.ano;
+				*max_cases_covid = covid_data_lst[i]->confirmados_arscentro - covid_data_lst[i - 1]->confirmados_arscentro;
+			}
+		}
+		break;
+	}
+	case 4:
+	{
+		for (int i = 1; i < cases; ++i)
+		{
+			int peak_day = covid_data_lst[i]->confirmados_arslvt - covid_data_lst[i - 1]->confirmados_arslvt;
+			if (peak_day > ant_peak_day && i > 0)
+			{
+				ant_peak_day = peak_day;
+				*max_dia = covid_data_lst[i]->data.dia;
+				*max_mes = covid_data_lst[i]->data.mes;
+				*max_ano = covid_data_lst[i]->data.ano;
+				*max_cases_covid = covid_data_lst[i]->confirmados_arslvt - covid_data_lst[i - 1]->confirmados_arslvt;
+			}
+		}
+		break;
+	}
+	case 5:
+	{
+		for (int i = 1; i < cases; ++i)
+		{
+			int peak_day = covid_data_lst[i]->confirmados_arsalentejo - covid_data_lst[i - 1]->confirmados_arsalentejo;
+			if (peak_day > ant_peak_day && i > 0)
+			{
+				ant_peak_day = peak_day;
+				*max_dia = covid_data_lst[i]->data.dia;
+				*max_mes = covid_data_lst[i]->data.mes;
+				*max_ano = covid_data_lst[i]->data.ano;
+				*max_cases_covid = covid_data_lst[i]->confirmados_arsalentejo - covid_data_lst[i - 1]->confirmados_arsalentejo;
+			}
+		}
+		break;
+	}
+	case 6:
+	{
+		for (int i = 1; i < cases; ++i)
+		{
+			int peak_day = covid_data_lst[i]->confirmados_arsalgarve - covid_data_lst[i - 1]->confirmados_arsalgarve;
+			if (peak_day > ant_peak_day && i > 0)
+			{
+				ant_peak_day = peak_day;
+				*max_dia = covid_data_lst[i]->data.dia;
+				*max_mes = covid_data_lst[i]->data.mes;
+				*max_ano = covid_data_lst[i]->data.ano;
+				*max_cases_covid = covid_data_lst[i]->confirmados_arsalgarve - covid_data_lst[i - 1]->confirmados_arsalgarve;
+			}
+		}
+		break;
+	}
+	case 7:
+	{
+		for (int i = 1; i < cases; ++i)
+		{
+			int peak_day = covid_data_lst[i]->confirmados_acores - covid_data_lst[i - 1]->confirmados_acores;
+			if (peak_day > ant_peak_day && i > 0)
+			{
+				ant_peak_day = peak_day;
+				*max_dia = covid_data_lst[i]->data.dia;
+				*max_mes = covid_data_lst[i]->data.mes;
+				*max_ano = covid_data_lst[i]->data.ano;
+				*max_cases_covid = covid_data_lst[i]->confirmados_acores - covid_data_lst[i - 1]->confirmados_acores;
+			}
+		}
+		break;
+	}
+	case 8:
+	{
+		for (int i = 1; i < cases; ++i)
+		{
+			int peak_day = covid_data_lst[i]->confirmados_madeira - covid_data_lst[i - 1]->confirmados_madeira;
+			if (peak_day > ant_peak_day && i > 0)
+			{
+				ant_peak_day = peak_day;
+				*max_dia = covid_data_lst[i]->data.dia;
+				*max_mes = covid_data_lst[i]->data.mes;
+				*max_ano = covid_data_lst[i]->data.ano;
+				*max_cases_covid = covid_data_lst[i]->confirmados_madeira - covid_data_lst[i - 1]->confirmados_madeira;
+			}
+		}
+		break;
+	}
+	}
+	return 0;
+}
 
-	printf("[%02d-%02d-%04d] Confirmados: %d\nIncrease: %d\n",
-		   records[peak_idx].date.day,
-		   records[peak_idx].date.month,
-		   records[peak_idx].date.year,
-		   records[peak_idx].confirmados,
-		   real);
+int peak_day_results(const int *max_dia, const int *max_mes, const int *max_ano, const int *max_cases_covid, const int idx)
+{
+	white();
+	switch (idx)
+	{
+	case 1:
+	{
+		printf("%d/%d/%d\t %d\t\t Nacional\n", *max_dia, *max_mes, *max_ano, *max_cases_covid);
+		break;
+	}
+	case 2:
+	{
+		printf("%d/%d/%d\t %d\t\t Norte\n", *max_dia, *max_mes, *max_ano, *max_cases_covid);
+		break;
+	}
+	case 3:
+	{
+		printf("%d/%d/%d\t %d\t\t Centro\n", *max_dia, *max_mes, *max_ano, *max_cases_covid);
+		break;
+	}
+	case 4:
+	{
+		printf("%d/%d/%d\t %d\t\t Lisboa e vale do Tejo\n", *max_dia, *max_mes, *max_ano, *max_cases_covid);
+		break;
+	}
+	case 5:
+	{
+		printf("%d/%d/%d\t %d\t\t Alentejo\n", *max_dia, *max_mes, *max_ano, *max_cases_covid);
+		break;
+	}
+	case 6:
+	{
+		printf("%d/%d/%d\t %d\t\t Algarve\n", *max_dia, *max_mes, *max_ano, *max_cases_covid);
+		break;
+	}
+	case 7:
+	{
+		printf("%d/%d/%d\t %d\t\t Acores\n", *max_dia, *max_mes, *max_ano, *max_cases_covid);
+		break;
+	}
+	case 8:
+	{
+		printf("%d/%d/%d\t %d\t\t Madeira", *max_dia, *max_mes, *max_ano, *max_cases_covid);
+		break;
+	}
+	}
+	reset();
+	return 0;
+}
+
+void release_covid_data(covid_data_t **covid_data_lst[])
+{
+	int i = 0;
+	while ((*covid_data_lst)[i] != NULL)
+	{
+		free((*covid_data_lst)[i]);
+		i++;
+	}
+	free(*covid_data_lst);
+	*covid_data_lst = NULL;
 }
 
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-		printf("Usage: %s <csv-file>\n", argv[0]);
-		return 0;
+		info();
+		printf("Usage: %s <filename>\n", argv[0]);
+		reset();
+		return 1;
 	}
-	printf("Using file %s\n", argv[1]);
-	FILE *input = fopen(argv[1], "r");
-	if (input == NULL)
+	char *filename = argv[1];
+
+	int max_dia = 0, max_mes = 0, max_ano = 0, max_cases = 0;
+
+	int tam_covid_lst = 0;
+
+	covid_data_t **covid_data_lst = NULL;
+	parse_covid_data(filename, &covid_data_lst, &tam_covid_lst);
+	blue();
+	printf("DATA:\t\t cases:\t\t REGIAO:\n");
+	reset();
+	int indice = 1;
+	for (int i = 0; i < 8; ++i)
 	{
-		printf("Cannot open file %s\n", argv[1]);
-		return 0;
+		peak_day(covid_data_lst, tam_covid_lst, &max_dia, &max_mes, &max_ano, &max_cases, indice);
+		peak_day_results(&max_dia, &max_mes, &max_ano, &max_cases, indice);
+		indice++;
 	}
-	// We can open the file
-	int num_records = 0;
-	covid_t data[MAX_RECORDS] = {0};
-	num_records = parse_file(input, data);
-	// display(data, num_records);
-	print_peak_day(data, num_records);
+	printf("\n");
+	release_covid_data(&covid_data_lst);
 	return 0;
 }
